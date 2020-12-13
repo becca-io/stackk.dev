d3b56b3c67999aac3757581e04bb16bc0b682f22
JaeYeopHan <ljyhanll@gmail.com>
2020-12-13 21:00:53 +0900

    feat: add next-auth route

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,23 @@
+import { Button } from '@chakra-ui/react';
 import Layout from 'components/Layout';
 import React from 'react';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export default function Main() {
+  const [session, loading] = useSession();
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
   return (
     <Layout fullHeight>
       <div>Main</div>
+      {session == null ? (
+        <Button onClick={() => signIn()}>로그인</Button>
+      ) : null}
+      {session != null ? (
+        <Button onClick={() => signOut()}>로그아웃</Button>
+      ) : null}
     </Layout>
   );
 }
